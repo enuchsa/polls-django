15c32748305db2fe193ab39d948312810e9a3acf
enuchsa <enuchsa@gmail.com>
2023-12-20 19:59:14 -0300

    fix name length in models

--- polls/models.py
+++ polls/models.py
@@ -7,5 +7,5 @@ class Question(models.Model):
     
 class Choice(models.Model):
     Question = models.ForeignKey(Question, on_delete=models.CASCADE)
-    choice_text = models.CharField(max_lenght=200)
+    choice_text = models.CharField(max_length=200)
     votes = models.IntegerField(default=0)
